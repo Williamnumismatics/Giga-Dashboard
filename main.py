2ca05d48af398d8c7811d151459c0800313d9d99
Williamnumismatics <williamnumismatics@gmail.com>
2025-10-19 12:11:55 +1100

    cpu temp for intel and amd

--- main.py
+++ main.py
@@ -13,3 +13,15 @@ except ImportError:
 
 HACKATIME_API_URL = "https://hackatime.hackclub.com/api/hackatime/v1"
 HACKATIME_API_KEY = "63c0a6d2-6bcd-46be-ae82-9e1a583d1833"
+
+def get_cpu_stats():
+    usage = psutil.cpu_percent(interval=1)
+    temp = 0.0
+    temps = psutil.sensors_temperatures()
+    if 'k10temp' in temps:
+        temp = temps['k10temp'][0].current
+    elif 'coretemp' in temps:
+        temp = sum(entry.current for entry in temps['coretemp']) / len(temps['coretemp'])
+    elif temps:
+        temp = next(iter(temps.values()))[0].current
+    return {"usage": usage, "temp": temp}
