311f8e45ea544aafbb3befe9643dbb541a221fcb
Williamnumismatics <williamnumismatics@gmail.com>
2025-10-19 12:03:41 +1100

    Update main.py
    Added more libraries but is inside a try becuase it is only applicable for windows

--- main.py
+++ main.py
@@ -3,3 +3,10 @@ import time
 import datetime
 import requests
 
+try:
+    from pycaw.pycaw import AudioUtilities, IAudioEndpointVolume
+    from ctypes import cast, POINTER
+    from comtypes import CLSCTX_ALL
+    is_Windows = True
+except ImportError:
+    is_Windows = False
